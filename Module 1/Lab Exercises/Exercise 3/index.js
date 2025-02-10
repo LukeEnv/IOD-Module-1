c601e47ec3d35e5bba180df372f28c5caddf0e87
LukeeRBLX <lukefranklin526@gmail.com>
2025-02-10 20:47:03 +1100

    Class 3

--- Module 1/Lab Exercises/Exercise 3/index.js
+++ Module 1/Lab Exercises/Exercise 3/index.js
@@ -1,10 +1,10 @@
 const colours = ["red", "green", "blue", "yellow", "purple", "orange"];
 
 function Column1() {
-  let NewColour = colours[Math.floor(Math.random() * colours.length)];
-  document.getElementById("column1").style.backgroundColor = NewColour;
+  let NewColour = colours[Math.floor(Math.random() * colours.length)]; // generate your colour
+  document.getElementById("column1").style.backgroundColor = NewColour; // apply the colour
   document.getElementById("heading1").textContent =
-    "Hello World! Colour is " + NewColour;
+    "Hello World! Colour is " + NewColour; // text here with the colour name
 }
 
 function Column2() {
